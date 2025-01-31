bfdd2bde979bcd94863dd21c06d6117738d6f1c9
reg <reg@regde-MacBook-Pro.local>
2025-01-31 15:42:09 +0800

    完成動態文字廣告後台製作

--- backend/ad.php
+++ backend/ad.php
@@ -12,23 +12,41 @@
         </tbody>
     </table>
     <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-        <p class="t cent botli">動態與文字廣告管理</p>
+        <p class="t cent botli">動態文字廣告管理</p>
         <form method="post"  action="./api/edit_<?=$do;?>.php">
             <table width="100%">
                 <tbody>
-                    <tr class="yel">
-                        <td width="45%">網站標題</td>
-                        <td width="23%">替代文字</td>
+                    <tr class="yel">                      
+                        <td width="23%">動態文字廣告</td>
                         <td width="7%">顯示</td>
                         <td width="7%">刪除</td>
-                        <td></td>
                     </tr>
+                    <?php
+                    $rows=$Ad->all();
+                    foreach($rows as $row){
+                    ?>
+                    <tr >                
+                        <td>
+                            <input type="text" name="text[]" value="<?=$row['text'];?>">
+                           </td>
+                        <td>
+                            <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked': '' ;?>>
+                           </td>
+                        <td>
+                            <input type="checkbox" name="del[]" value="<?=$row['id'];?>"> 
+                          </td>
+                        <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                    </tr>
+                    <?php
+                    }
+                    ?>
                 </tbody>
             </table>
             <table style="margin-top:40px; width:70%;">
                 <tbody>
                     <tr>
-                        <td width="200px"><input type="button"
+                        <td width="200px">
+                            <input type="button"
                                 onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/<?=$do;?>.php&#39;)"
                                 value="新增動態文字廣告"></td>
                         <td class="cent">
